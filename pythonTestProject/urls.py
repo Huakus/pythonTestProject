ee95effac086469f1e31ba668e0352bce00854f3
Joaquin Senn <joaquinsenn@gmail.net>
2023-04-13 21:51:34 +0200

    Added serializer dependency injection

--- pythonTestProject/urls.py
+++ pythonTestProject/urls.py
@@ -2,8 +2,9 @@ from django.contrib import admin
 from django.urls import path
 from zipcodes.views import get_location_info
 from zipcodes.services import BasicLocationService
+from zipcodes.serializers import BasicLocationSerializer
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('location/<str:zip_code>/', lambda request, zip_code: get_location_info(request, zip_code, BasicLocationService()), name='get_location_info'),
+    path('location/<str:zip_code>/', lambda request, zip_code: get_location_info(request, zip_code, BasicLocationService(), BasicLocationSerializer), name='get_location_info'),
 ]
--- zipcodes/views.py
+++ zipcodes/views.py
@@ -1,21 +1,17 @@
 from django.shortcuts import render
 from django.http import Http404
-from django.conf import settings
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from .serializers import LocationSerializer
 from .services import ILocationService
 import logging
 
 logger = logging.getLogger(__name__)
 
 @api_view(['GET'])
-def get_location_info(request, zip_code, location_service: ILocationService):
+def get_location_info(request, zip_code, location_service: ILocationService, location_serializer_class):
     try:
-        zip_code = 90210
-
         location_info = location_service.get_location_info(zip_code)
-        location_serializer = LocationSerializer(data=location_info)
+        location_serializer = location_serializer_class(data=location_info)
 
         if location_serializer.is_valid():
             location_serializer.save()
