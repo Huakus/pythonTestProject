27f1cdbb53883f0a6a58a2202733f11b23516d0f
Joaquin Senn <joaquinsenn@gmail.net>
2023-04-14 16:38:08 +0200

    Added validation to avoid going to the API if it already exists on the DB

--- zipcodes/views.py
+++ zipcodes/views.py
@@ -3,6 +3,7 @@ from django.http import Http404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .services import ILocationService
+from .models import Location
 import logging
 
 logger = logging.getLogger(__name__)
@@ -10,11 +11,16 @@ logger = logging.getLogger(__name__)
 @api_view(['GET'])
 def get_location_info(request, zip_code, location_service: ILocationService, location_serializer_class):
     try:
-        location_info = location_service.get_location_info(zip_code)
-        location_serializer = location_serializer_class(data=location_info)
+        try:
+            location = Location.objects.get(zip_code=zip_code)
 
-        location_serializer.validate_and_save()
+        except Location.DoesNotExist:
+            logger.info(f"New Location for zip_code {zip_code}")
+            location = location_service.get_location_info(zip_code)
+            location_serializer = location_serializer_class(data=location)
+            location = location_serializer.validate_and_save()
 
+        location_serializer = location_serializer_class(location)
         return Response(location_serializer.data)
     except Exception as e:
         logger.error('Exception getting location info: %s', str(e), exc_info=1)
