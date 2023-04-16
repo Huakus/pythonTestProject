04c94e14351d1c7f8a911a3effd539c9cf4b233e
Joaquin Senn <joaquinsenn@gmail.net>
2023-04-16 13:22:33 +0200

    Improved location existence validation

--- zipcodes/views.py
+++ zipcodes/views.py
@@ -18,14 +18,12 @@ def get_location_info(request, zip_code, location_service: ILocationService, loc
     try:
         try:
             location = Location.objects.get(zip_code=zip_code)
-
+            location_serializer = location_serializer_class(location)
         except Location.DoesNotExist:
             logger.info(f'New Location for zip_code {zip_code}')
             location = location_service.get_location_info(zip_code)
             location_serializer = location_serializer_class(data=location)
             location = location_serializer.validate_and_save()
-
-        location_serializer = location_serializer_class(location)
         return Response(location_serializer.data)
     except Exception as e:
         logger.error('Exception getting location info: %s', str(e), exc_info=1)
